f4fe52d7cfaecad3846fd26a088b9eb387485640
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-09-23 22:39:15 +0200

    Update bc_price_index.py

--- bc_price_index.py
+++ bc_price_index.py
@@ -1,49 +1,31 @@
 #!/usr/bin/env python3
-import requests
 
-class bitcoin_price_index:
+# This is a python implementation of the CoinDesk API
+# For getting Bitcoin price index
+
+# Importing required modules
+import requests
+from pprint import pprint
+
+class PriceIndex:
     def __init__(self):
         # Get request fetching the latest bitcoin price index in json format
         self.response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
 
     def on_connection(self):
-        print(f"""
-{self.response['chartName']}
-├ {self.response['bpi']['EUR']['code']}
-├─ Rate: {self.response['bpi']['EUR']['rate']}
-├── Rate Float: {self.response['bpi']['EUR']['rate_float']}
-├─ Symbol: {self.response['bpi']['EUR']['symbol']}
-└─╼ Desription: {self.response['bpi']['EUR']['description']}  
-├ {self.response['bpi']['GBP']['code']}
-├─ Rate: {self.response['bpi']['GBP']['rate']}
-├── Rate Float: {self.response['bpi']['GBP']['rate_float']}
-├─ Symbol: {self.response['bpi']['GBP']['symbol']}
-└─╼ Desription: {self.response['bpi']['GBP']['description']}
-├ {self.response['bpi']['USD']['code']}
-├─ Rate: {self.response['bpi']['USD']['rate']}
-├── Rate Float: {self.response['bpi']['USD']['rate_float']}
-├─ Symbol: {self.response['bpi']['USD']['symbol']}
-└─╼ Desription: {self.response['bpi']['USD']['description']}
-├ {self.response['bpi']['GBP']['code']}
-├─ Rate: {self.response['bpi']['GBP']['rate']}
-├── Rate Float: {self.response['bpi']['GBP']['rate_float']}
-├─ Symbol: {self.response['bpi']['GBP']['symbol']}
-└─╼ Desription: {self.response['bpi']['GBP']['description']}
-├ Updated on: {self.response['time']['updated']}
-├─ Updated ISO: {self.response['time']['updatedISO']}
-├── Updated UK: {self.response['time']['updateduk']}
-└─╼ Disclaimer: {self.response['disclaimer']}""")
-
+        # The pprint module provides a capability 
+        # to “pretty-print” arbitrary Python data 
+        # structures in a well-formatted and more readable way.
+        pprint(self.response)
+        
 if __name__ == "__main__":
-	print("Connecting...")
+    # Main loop
 	while True:
 		try:
-			bitcoin_price_index().on_connection()
+			PriceIndex().on_connection()
 			break
-			
 		except Exception as e:
 			print(f"An error occured: {e}")
-			print("Reconnecting...")
-			
+			print("Retrying...")
 		except KeyboardInterrupt:
 			exit()
